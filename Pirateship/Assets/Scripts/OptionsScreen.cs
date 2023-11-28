6626cd366a127381f927df31e2b45bbd8336194a
Hiquers <riquesouzalima11@gmail.com>
2023-11-28 17:15:55 -0300

    UI improvements

--- Pirateship/Assets/Scripts/OptionsScreen.cs
+++ Pirateship/Assets/Scripts/OptionsScreen.cs
@@ -20,27 +20,37 @@ public class OptionsScreen : MonoBehaviour
     
     [SerializeField]
     private GameConfig _gameConfig;
-   
+
+    private int _minutes;
+    private int _seconds;
 
     public void WhenGameSlideChange()
     {
         _gameConfig._timeOfGame =  _gameTimeSlider.value;
-        _gameTimeText.text = _gameConfig._timeOfGame.ToString();
+        FormatFloatToMin();
+        _gameTimeText.text = string.Concat(_minutes.ToString("00"), ":", _seconds.ToString("00"),"min");
 
     }
     public void WhenSpawnSlideChange()
     {
         _gameConfig._secondsToSpawn = _spawnTimeSlider.value/10;
-        _spawnTimeText.text = _gameConfig._secondsToSpawn.ToString("0.0");
+        _spawnTimeText.text = string.Concat( _gameConfig._secondsToSpawn.ToString("0.0"), "s");
     }
     
     void Start()
     {
         _gameTimeSlider.value = _gameConfig._timeOfGame;
-        _gameTimeText.text = _gameConfig._timeOfGame.ToString();
+        FormatFloatToMin();
+        _gameTimeText.text = string.Concat(_minutes.ToString("00"), ":", _seconds.ToString("00"), "min");
 
         _spawnTimeSlider.value = _gameConfig._secondsToSpawn * 10;
-        _spawnTimeText.text = _gameConfig._secondsToSpawn.ToString("0.0");
+        _spawnTimeText.text = string.Concat(_gameConfig._secondsToSpawn.ToString("0.0"), "s");
+    }
+
+    private void FormatFloatToMin()
+    {
+        _minutes = (int)_gameConfig._timeOfGame / 60;
+        _seconds = (int)_gameConfig._timeOfGame % 60;
     }
 
     
